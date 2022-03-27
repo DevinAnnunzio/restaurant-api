eecd070d1a548491451829d8576a19db270092ce
DevinAnnunzio <annunzio.da@gmail.com>
2022-03-27 23:31:00 +0900

    Feat: DB schema updated

--- db/migrations/20220327132525_init.js
+++ db/migrations/20220327132525_init.js
@@ -7,6 +7,8 @@ exports.up = function(knex) {
         table.increments('order_id');
         table.integer('table_number').notNullable();
         table.string('food').notNullable();
+        table.boolean('cooked').notNullable();
+        table.timestamps(true, true);
     })
   
 };
